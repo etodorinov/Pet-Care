4b6b699ae5d67f139bd57b39cc79bc02edbac725
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-04-30 11:37:37 +0300

    Add files via upload

--- solution/views/homeView.js
+++ solution/views/homeView.js
@@ -1,11 +1,19 @@
-import { html } from "../../node_modules/lit-html/lit-html.js";
+// import { html } from "../../node_modules/lit-html/lit-html.js";
+import { html } from "https://unpkg.com/lit-html@1.3.0/lit-html.js";
 
 const homeViewTemplate = () => html` <section class="welcome-content">
   <article class="welcome-content-text">
     <h1>We Care</h1>
     <h1 class="bold-welcome">Your Pets</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.
+      This is my solution of the task given on SoftUni JavaScript Applications
+      exam conducted on 02 April 2022.
+    </p>
+    <p position="absolute">
+      <span>
+        <a href="/requirements">Pet Care_requirements</a>
+        (clicking on the link will open a new tab which will display a .pdf file with the requirements for this SPA (Single Page Application)).</span>
+        <p>Link to <a href="/github">GitHub</a> for code review may be provided upon request.</p>
     </p>
   </article>
   <article class="welcome-content-image">
