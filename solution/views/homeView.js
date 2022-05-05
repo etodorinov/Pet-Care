f72d6283368e109f42a54a633746af2e5b08ae60
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-05-06 00:00:23 +0300

    Add files via upload

--- solution/views/homeView.js
+++ solution/views/homeView.js
@@ -5,25 +5,23 @@ const homeViewTemplate = () => html` <section class="welcome-content">
   <article class="welcome-content-text">
     <h1>We Care</h1>
     <h1 class="bold-welcome">Your Pets</h1>
-    <p>
+    <p class="content">
       This is my solution of the task for creation of SPA (Single Page
-      Application) given on <a href="/softuni">SoftUni</a> JavaScript Applications
-      <a href="/exam">exam</a> conducted on 02 April 2022.
+      Application) given on <a href="/softuni">SoftUni</a> JavaScript
+      Applications <a href="/exam">exam</a> conducted on 02 April 2022.
     </p>
-    <p>
+    <p class="content">
       The application is hosted on <a href="/heroku">Heroku</a> and
       <a href="/firebase">Firebase</a> combined so the initial load of the
       functionalities may take little longer as it takes some time to "wake up"
       the servers.
     </p>
-    <p position="absolute">
-      <span
-        ><a href="/requirements">Pet Care_requirements</a> - clicking on the
-        link will open a new tab which will display a .pdf file with the
-        requirements for this SPA (Single Page Application).</span
-      >
+    <p class="content">
+      <a href="/requirements">Pet Care_requirements</a> - clicking on the link
+      will open a new tab which will display a .pdf file with the requirements
+      for this SPA (Single Page Application).
     </p>
-    <p>
+    <p class="content">
       Link to <a href="/github">GitHub</a> for code review may be provided upon
       request.
     </p>
